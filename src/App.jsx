f0ff14437327e4eb214d85fdbfb8bb17e66320f8
Prosudomaster <98703534+Prosudomaster@users.noreply.github.com>
2025-11-06 00:53:34 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import NavBar from './components/NavBar'
+import HeroSection from './sections/HeroSection'
+import { ScrollTrigger } from 'gsap/all'
+import gsap from 'gsap';
+import MessageSection from './sections/MessageSection';
+import FlavorSection from './sections/FlavorSection';
+import { ScrollSmoother } from 'gsap/ScrollSmoother';
+import { useGSAP } from '@gsap/react';
+import NutritionSection from './sections/NutritionSection';
+import BenefitSection from './sections/BenefitSection';
+import TestimonialSection from './sections/TestimonialSection';
+import FooterSection from './sections/FooterSection';
+
+gsap.registerPlugin(ScrollTrigger,ScrollSmoother);
+
+const App = () => {
+
+  useGSAP(()=>{
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+    })
+  })
+
+  return (
+    <main>
+      <NavBar/>
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+      <HeroSection/>
+      <MessageSection/>
+      <FlavorSection/>
+      <NutritionSection/>
+
+      <div className="">      
+      <BenefitSection/>
+      <TestimonialSection/>
+      </div>
+      <FooterSection/>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
